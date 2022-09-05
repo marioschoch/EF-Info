ecef3bad83023b17f7c9f8efe5c53fff8761dc8c
marioschoch <marioschoch05@gmail.com>
2022-09-05 13:44:59 +0200

    Add versuche Primzahlen printen

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -5,5 +5,15 @@
 # Print if modulo = > 0
 
 
-for index in range(0, 100):
-    print(if(index%))
+
+#for index in range(1, 100):
+#    print(index%(index-1))
+
+PRIME = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+for i in range(1, 101, 2):
+    for n in PRIME:
+         if i % n == 0:
+            print (i)
+
+
