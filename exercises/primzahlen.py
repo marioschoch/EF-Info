a08bbbc43b51c1fd429f58e55ef69b87cd8164fe
marioschoch <marioschoch05@gmail.com>
2022-09-10 19:38:18 +0200

    Add lösungansatz

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -9,11 +9,13 @@
 #for index in range(1, 100):
 #    print(index%(index-1))
 
-PRIME = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+#PRIME = [2, 3, 5, 7]                       #versuch es selbst zu machen
 
-for i in range(1, 101, 2):
-    for n in PRIME:
-         if i % n == 0:
-            print (i)
+#for i in range(1, 101, 2):
+#    for n in PRIME:
+#         if i % n == 0:
+#            print(i)
 
+# Lösung
 
+primes = []
