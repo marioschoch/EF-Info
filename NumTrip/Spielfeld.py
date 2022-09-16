01975bd8bb5529a5e638392b915cc3afe42b944a
marioschoch <marioschoch05@gmail.com>
2022-09-16 13:55:32 +0200

    add spielfeld anfang

---
+++ NumTrip/Spielfeld.py
@@ -0,0 +1,17 @@
+
+Spielfeld =[[2,4,4,4,2],[12,2,4,2,2],[2,2,4,4,2],[2,4,4,4,4],[4,4,4,4,4]]
+
+#print('hallo', Spielfeld[0][0])
+#print(f'hallo      {Spielfeld[0][0]}', )  #formatiert
+
+for zeile in Spielfeld:
+    print('+------+------+------+------+------+')
+    print('|      |      |      |      |      |')
+    if zeile[0] <= 10:
+        print(f'|   {(zeile[0])}  |', end='')
+    else:
+        print(f'|  {(zeile[0])}  |', end='')
+    print('|      |      |      |      |      |')
+print('+------+------+------+------+------+')
+    
+
