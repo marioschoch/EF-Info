56de121b48ffcc4289135336f134c291db845063
marioschoch <marioschoch05@gmail.com>
2025-03-19 15:23:24 +0100

    add faculty

--- docs/Algorithmen/faculty.py
+++ docs/Algorithmen/faculty.py
@@ -6,8 +6,8 @@ zahl_zu_faktorisieren = 5
 
 n = zahl_zu_faktorisieren
 def factorial(n):
-    if (n==1):
-       return n
+    if (n==0):
+       return 1
     else:
         return n*factorial(n-1)
 
